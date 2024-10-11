1218e0ec8b80341b71e7f3c2c6a2940f45d2eef1
SF <sf2023@github.com>
2024-10-11 10:55:13 -0500

    made the api request, format and basic error handling for openstreetmap

--- api_maps.py
+++ api_maps.py
@@ -1,16 +1,53 @@
 import requests
 
 #when the user inputs data into program
-#you will be reciving national park name as string name
-#also reciving city and state name as string is this format "minneapolis, mn"
+#you will be receiving national park name as string name
+#also receiving city and state name as string is this format "minneapolis, mn"
 
 
 def getMaps(location):
     # location is the name of the city and state
-    print(location)
+    print(f'Getting directions data for: {location}')
 
+    url = 'https://nominatim.openstreetmap.org/search'
+    # Nominatim API is the geocoding service for OSM
 
-    #test variable to test if function works
-    test = "test"
-    
-    return test
+    params = {
+        'q' : location,
+        'format': 'json',
+        'address': 1,
+        'limit': 1,
+    }
+    # request parameters
+
+    try:
+        response = requests.get(url,params=params)
+        response.raise_for_status()
+        # API request
+        # raise errors for unwanted responses
+        data = response.json()
+        # check for results
+
+        if data:
+            # retrieve information
+            starting_location = data[0]
+            home_address = starting_location['display_name']
+            return {
+                'home_address': home_address,
+                'city': starting_location.get('address', {}).get('city', ''),
+                'country': starting_location.get('address', {}).get('country', '')
+            #     so the user can input city and country instead of lat and lon
+            }
+        else:
+            return {'error': 'Location not found.'}
+    except requests.exceptions.RequestException as e:
+        print(f'Error fetching map data: {e}')
+        return {'error': str(e)}
+#     error handling
+
+if __name__ == '__main__':
+    location = 'Minneapolis, USA'
+    # input format
+    result = getMaps(location)
+    print(result)
+
