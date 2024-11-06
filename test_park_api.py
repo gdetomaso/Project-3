b5ef0fe10a153744e53a9b8f100a82f785f35cbc
m <example@example.com>
2024-11-05 21:06:37 -0600

    added 2 unit tests for park api

--- test_park_api.py
+++ test_park_api.py
@@ -1,13 +1,26 @@
 from unittest import TestCase
-import os
+from unittest.mock import patch
 from api_nps import get_national_park_info_from_api
 
 
-class TestParkAPI(TestCase):
+class TestNationalParksInfo(TestCase):
+    @patch('api_nps.get_national_park_info_from_api')
+    def test_park_name(self, mock_park_info):  # mock_park_info takes place of get_national_park_info_from_api
+        example_park_info_response = {"fullName": "Yellowstone National Park"}  # mock JSON response
+        mock_park_info.return_value = [example_park_info_response] # return value is equal to mock response
+        result, _ = get_national_park_info_from_api("yell") # call function with correct four-letter code
+        expected = "Yellowstone National Park"
+        self.assertEqual(expected, result.name)
 
-   def test_known_park(self):
-    test_park_known = 
-    self.assertEqual(test_park_known, 'YELL')
+
+    @patch('api_nps.get_national_park_info_from_api')
+    def test_unknown_park(self, mock_park_info):
+        unknown_park_example_response = {"fullName": None}
+        mock_park_info.return_value = [unknown_park_example_response]
+        result, _ = get_national_park_info_from_api(None)
+        expected = None
+        self.assertEqual(expected, result)
+
 
 
 
